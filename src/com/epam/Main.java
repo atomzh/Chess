801b079dd32d96d1301d679bb4cacae384da8830
Ислам <51363562+atomzh@users.noreply.github.com>
2019-07-05 01:34:08 +0600

    update

--- src/com/epam/Main.java
+++ src/com/epam/Main.java
@@ -1,8 +1,14 @@
 package com.epam;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Введите N-Число верзей и N х N клеток");
+        Scanner scanner = new Scanner(System.in);
+        String n = scanner.next();
+        Queen queen = new Queen(Integer.parseInt(n));
+
     }
 }
---
+++ src/com/epam/Queen.java
@@ -0,0 +1,9 @@
+package com.epam;
+
+public class Queen {
+    private int nQueen;
+
+    public Queen(int nQueen) {
+        this.nQueen = nQueen;
+    }
+}
